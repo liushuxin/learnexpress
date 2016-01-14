91448665198f7ca09add20fad1b05ef6ce28dc6f
liushuxin <liushuxin@liushuxindeMacintosh.local>
2016-01-14 19:29:13 +0800

    修改highchart配置信息

--- public/javascripts/lodash_test.js
+++ public/javascripts/lodash_test.js
@@ -45,28 +45,44 @@ var arr2 = [ 
   { id: 3, group_id: 1 },
   { id: 4, group_id: 2 } 
 ];
-
+var category =['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun','Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 var group =_.groupBy(arr2, 'group_id')
 console.log(group);
 
 console.log($);
 //折线图
 $('#chart_line').highcharts({
-  type:'bar',
+        chart: {
+            type: "spline",
+            style: {
+                fontFamily: "",
+                fontSize: '12px',
+                fontWeight: 'bold',
+                color: '#006cee'
+            },
+            zoomType:'xy'
+        },
+
         title: {
             text: '月平均温度',
-            x: -20 //center
+            x: -20 ,//center
+            useHTML:true,
+            style:{
+                color:'gold'
+            }
         },
         subtitle: {
             text: 'Source: WorldClimate.com',
             x: -20
         },
         xAxis: {
-            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun','Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+            categories:category,
+            //gridLineWidth:'1',
+            //gridLineDashStyle:'Dash',
         },
         yAxis: {
             title: {
-                text: 'Temperature (°C)'
+                text: '温度 (°C)'
             },
             plotLines: [{
                 value: 0,
@@ -75,7 +91,21 @@ $('#chart_line').highcharts({
             }]
         },
         tooltip: {
-            valueSuffix: '°C'
+            shared:true,
+            crosschairs:true,
+            valueSuffix: '°C',
+            //backgroundColor: '#FCFFC5', // 背景颜色
+            //borderColor: 'black', // 边框颜色
+            borderRadius: 10, // 边框圆角
+            borderWidth: 3, // 边框宽度
+            shadow: true, // 是否显示阴影
+            animation: true, // 是否启用动画效果
+            style: { // 文字内容相关样式
+             //   color: "#ff0000",
+                fontSize: "12px",
+                fontWeight: "blod",
+                fontFamily: "Courir new"
+            }
         },
         legend: {
             layout: 'horizontal',
@@ -85,18 +115,24 @@ $('#chart_line').highcharts({
              x: 300, 
             y: 20
         },
-        series: [{
+        series: [
+            
+        {
             name: 'Tokyo',
-            data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
+            data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6],
+            
         }, {
             name: 'New York',
-            data: [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]
+            data: [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5],
+             
         }, {
             name: 'Berlin',
-            data: [-0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0]
+            data: [-0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0],
+       
         }, {
             name: 'London',
-            data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
+            data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8],
+            
         }]
     });
 
